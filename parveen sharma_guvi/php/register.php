2ca68969402513922df73be0bf361316f6144e01
praveen22sharma <113980715+praveen22sharma@users.noreply.github.com>
2022-11-12 13:06:04 +0530

    Add files via upload

---
+++ parveen sharma_guvi/php/register.php
@@ -0,0 +1,42 @@
+<?php
+	
+	if($showAlert) {
+	
+		echo ' <div class="alert alert-success
+			alert-dismissible fade show" role="alert">
+	
+			<strong>Success!</strong> Your account is
+			now created and you can login.
+			<button type="button" class="close"
+				data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">×</span>
+			</button>
+		</div> ';
+	}
+	
+	if($showError) {
+	
+		echo ' <div class="alert alert-danger
+			alert-dismissible fade show" role="alert">
+		<strong>Error!</strong> '. $showError.'
+	
+	<button type="button" class="close"
+			data-dismiss="alert aria-label="Close">
+			<span aria-hidden="true">×</span>
+	</button>
+	</div> ';
+}
+		
+	if($exists) {
+		echo ' <div class="alert alert-danger
+			alert-dismissible fade show" role="alert">
+	
+		<strong>Error!</strong> '. $exists.'
+		<button type="button" class="close"
+			data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">×</span>
+		</button>
+	</div> ';
+	}
+
+?>
